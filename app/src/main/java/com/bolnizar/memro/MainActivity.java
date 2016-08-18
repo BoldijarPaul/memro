71b146d3c5c0b1b7cd00603315e2fbc8d3f0ab58
Boldijar Paul <paul.boldijar@appsrise.com>
2016-08-18 10:26:39 +0300

    added search menu

--- app/src/main/java/com/bolnizar/memro/MainActivity.java
+++ app/src/main/java/com/bolnizar/memro/MainActivity.java
@@ -1,7 +1,11 @@
 package com.bolnizar.memro;
 
+import com.bolnizar.memro.fragments.SearchFragment;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new SearchFragment()).commit();
     }
 }
